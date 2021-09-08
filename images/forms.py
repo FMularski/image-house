41003694d28b16412f3149e75f169b7f789edd47
Filip Mularski <mularskif@gmail.com>
2021-09-08 23:28:25 +0200

    implemented signing up and signing in

--- images/forms.py
+++ images/forms.py
@@ -6,12 +6,13 @@ from . import models
 
 class SignUpForm(UserCreationForm):
 
-    password1 = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password', max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm password', max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = get_user_model()
         fields = 'username', 'first_name', 'last_name', 'email'
+
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
--- images/views.py
+++ images/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages
+from django.http import JsonResponse
 from . import forms
 
 
-def sign_up(request):
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect(reverse('home', ))
+        
+        messages.error(request, 'Invalid credentials.')
+
     context = {}
+    return render(request, 'images/sign_in.html', context)
+
+
+def sign_up(request):
+    form = forms.SignUpForm()
+
+    if request.method == 'POST':
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'User \'{form.cleaned_data.get("username")}\' has been signed up.')
+            return redirect(reverse('sign_in', ))
+
+    context = {'form': form}
     return render(request, 'images/sign_up.html', context)
+
+
+@login_required(login_url='sign_in')
+def home(request):
+    return JsonResponse(data={'status': 200})
