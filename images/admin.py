42915e61897f81b21ea282b5ddff70270673cc4f
Filip Mularski <mularskif@gmail.com>
2021-09-10 23:10:19 +0200

    created models: Category, Image and their ModelAdmins

--- images/admin.py
+++ images/admin.py
@@ -3,6 +3,21 @@ from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = 'title', 'category', 'user', 'created_at'
+    ordering = 'created_at',
+
+
+
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Image, ImageAdmin)
+
+
+
 
 
--- images/models.py
+++ images/models.py
@@ -6,3 +6,23 @@ class User(AbstractUser):
     pass
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    img = models.ImageField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    views = models.PositiveIntegerField(default=0)
+    votes = models.IntegerField(default=0)
+
+
+
+
+
